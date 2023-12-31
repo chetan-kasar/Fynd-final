5530b1b4be9792a70864968190a6fdc3802d6ac1
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-31 23:39:17 +0530

    Update UserContent.js

--- travel-log-app/src/Components/UserContent.js
+++ travel-log-app/src/Components/UserContent.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const UserContent = (props) => {
 
+  const styleMargin = {
+    marginTop:"23px",
+  }
+  
   const flightStyle = {
     height:"100px",
-    width:"100px"
+    width:"100px",
+    marginTop:"23px",
   }
 
   return (
@@ -13,32 +18,32 @@ const UserContent = (props) => {
 
         <div className="child2">
           <div className="heading">Title</div>
-          <h1>{props.userData.titleData.title}</h1>
+          <h1 style={styleMargin}>{props.userData.titleData.title}</h1>
         </div>
 
         <div className="child2">
           <div className="heading">Weather</div>
-          <img src={props.userData.weatherData.weatherImg}/>
+          <img src={props.userData.weatherData.weatherImg style={styleMargin}}/>
           <h1>{props.userData.weatherData.weather}</h1>
           <h1>{props.userData.weatherData.temprature} &#8451;</h1>
         </div>
 
         <div className="child2">
           <div className="heading">Note</div>
-          <p className="text-content">{props.userData.textContent.content}</p>
+          <p className="text-content" style={styleMargin}>{props.userData.textContent.content}</p>
         </div>
 
         <div className="child3">
           <div className="heading">Flight</div>
 
-          <div className='child3-s1'>
+          <div className='child3-s1' style={styleMargin}>
             <p className="">{props.userData.flightData.fromInput}</p>
             <p className="">{props.userData.flightData.departure}</p>
           </div>
 
           <img style={flightStyle} src={require("../Icons/airplane.png")}/>
 
-          <div className='child3-s2'>
+          <div className='child3-s2' style={styleMargin}>
             <p className="">{props.userData.flightData.toInput}</p>
             <p className="">{props.userData.flightData.destination}</p>
           </div>
@@ -48,12 +53,35 @@ const UserContent = (props) => {
           <div className="heading">Photos</div>
           {
             props.userData.albumData.photos.map((ele)=>{
-              return (<img className="photo-sec" src={`data:image/jpeg;base64,${ele.e}`}/>);
+              return (<img className="photo-sec" src={`data:image/jpeg;base64,${ele.e}`} style={styleMargin}/>);
             })
 
           }
         </div>
 
+        <div className='child4'>
+          <div className='heading'>Locations</div>
+          <MapContainer center={center} zoom={5} className='map-style' style={styleMargin}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            {
+              props.userData.mapData.map((ele)=>{
+                return (
+                  <Marker position={ele} icon={customIcon}>
+                    <Popup>
+                      <div>
+                        <h1>My location</h1>
+                      </div>
+                    </Popup>
+                  </Marker>
+                )
+              })
+            }
+          </MapContainer>
+          </div>
       </div>
     </div>
   )
